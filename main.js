53f0c9dbde3ff9bcd52c7df382526a4c01d02654
simons129 <servatka29@gmail.com>
2020-04-04 12:06:55 +0200

    Update main.js
    Mail skúška

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 window.onresize = function(){ location.reload(); }
 
-
-var token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOlsidXNlciIsIm1haWwiLCJzdG9yZSJdLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiNWM2MTBiNDlmZGE5ZGIyYzA4MGJmNDgzIiwiZXhwIjoxNTg2NjI4NTUwLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjUwMDYxLyIsImF1ZCI6Imh0dHA6Ly9sb2NhbGhvc3Q6NTAwNjEvIn0.sb_ybk24nafWoCSj9m3_pJ6FSxz0xf68E_exSZXRY2U';
-
 $(document).ready(function () {
   $("form").submit(handleSubmit);
 });
@@ -10,24 +7,28 @@ $(document).ready(function () {
 function handleSubmit(event) {
   event.preventDefault();
 
-  var msg = $('#msg').val();
+  var msg = "<p>" + $('#msg').val() + "</p>";
   var email = $('#email').val();
 
   var data = {
     to: "servatka.simon@gmail.com",
-    subject: "Kontakt z simons129.github.io",
-    body: msg,
-    from: email
+    subject: "simons129.github.io",
+    html: msg,
+    company: email, 
+    sendername: ""
   }
 
   $.ajax({
     type: "POST",
-    url: "https://stored.azurewebsites.net/api/mail",
+    url: "https://simons129-2094.restdb.io/mail",
     headers: {
-      "Authorization": 'Bearer ' + token
+      "x-apikey": 'c9fdb0cc10b56c67055557befc2da3663d83b',
+      "Catche-Control": "no-cache"
     },
     data: JSON.stringify(data),
-    contentType: "application/json"
+    contentType: "application/json",
+    Host: 'simons129-2094.restdb.io'
+    
   })
     .done(function () {
       console.log('OK');
