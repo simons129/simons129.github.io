6ad50531f7a6b0d1c01401f355615729a4b341e6
simons129 <servatka29@gmail.com>
2020-04-07 15:48:40 +0200

    Revert "Update main.js"
    This reverts commit 53f0c9dbde3ff9bcd52c7df382526a4c01d02654.

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 window.onresize = function(){ location.reload(); }
 
+
+var token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOlsidXNlciIsIm1haWwiLCJzdG9yZSJdLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiNWM2MTBiNDlmZGE5ZGIyYzA4MGJmNDgzIiwiZXhwIjoxNTg2NjI4NTUwLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjUwMDYxLyIsImF1ZCI6Imh0dHA6Ly9sb2NhbGhvc3Q6NTAwNjEvIn0.sb_ybk24nafWoCSj9m3_pJ6FSxz0xf68E_exSZXRY2U';
+
 $(document).ready(function () {
   $("form").submit(handleSubmit);
 });
@@ -7,28 +10,24 @@ $(document).ready(function () {
 function handleSubmit(event) {
   event.preventDefault();
 
-  var msg = "<p>" + $('#msg').val() + "</p>";
+  var msg = $('#msg').val();
   var email = $('#email').val();
 
   var data = {
     to: "servatka.simon@gmail.com",
-    subject: "simons129.github.io",
-    html: msg,
-    company: email, 
-    sendername: ""
+    subject: "Kontakt z simons129.github.io",
+    body: msg,
+    from: email
   }
 
   $.ajax({
     type: "POST",
-    url: "https://simons129-2094.restdb.io/mail",
+    url: "https://stored.azurewebsites.net/api/mail",
     headers: {
-      "x-apikey": 'c9fdb0cc10b56c67055557befc2da3663d83b',
-      "Catche-Control": "no-cache"
+      "Authorization": 'Bearer ' + token
     },
     data: JSON.stringify(data),
-    contentType: "application/json",
-    Host: 'simons129-2094.restdb.io'
-    
+    contentType: "application/json"
   })
     .done(function () {
       console.log('OK');
