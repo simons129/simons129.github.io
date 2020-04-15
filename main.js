a49897aa836440abc3cb668b5cc8c0bfb69da7c6
simons129 <servatka29@gmail.com>
2020-04-15 11:10:59 +0200

    refactor ajax

--- main.js
+++ main.js
@@ -1,32 +1,46 @@
-window.onresize = function(){ location.reload(); }
+window.onresize = function () { location.reload(); }
 
-$(document).ready(function () {
-  $("form").submit(handleSubmit);
+window.addEventListener("DOMContentLoaded", function () {
+
+  // get the form elements defined in your form HTML above
+
+  var form = document.getElementById("my-form");
+  var button = document.getElementById("my-form-button");
+  var status = document.getElementById("my-form-status");
+
+  // Success and Error functions for after the form is submitted
+
+  function success() {
+    form.reset();
+    document.getElementsByClassName("alert")[0].style.display = "block";
+  }
+
+  function error() {
+    document.getElementsByClassName("alert")[1].style.display = "block";
+  }
+
+  // handle the form submission event
+
+  form.addEventListener("submit", function (ev) {
+    ev.preventDefault();
+    var data = new FormData(form);
+    ajax(form.method, form.action, data, success, error);
+  });
 });
 
-function handleSubmit(event) {
-  event.preventDefault();
+// helper function for sending an AJAX request
 
-  var msg = $('#msg').val();
-  var email = $('#email').val();
-
-  var data = {
-    email,msg
-  }
-  data = $.param(data)
-
-  $.ajax({
-    type: "POST",
-    url: "https://formspree.io/xgeldavr",
-    data: JSON.stringify(data),
-    header: {'Accept': 'application/json'}
-  })
-    .done(function () {
-      console.log('OK');
-      document.getElementsByClassName("alert")[0].style.display = "block";
-    })
-    .fail(function (error) {
-      console.log(error);
-      document.getElementsByClassName("alert")[1].style.display = "block";
-    });
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
 }
